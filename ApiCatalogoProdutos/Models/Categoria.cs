e629ab51c58ae94911dad0623492241fb0d62be0
elvis.alves54@gmail.com <elvis.alves54@gmail.com>
2021-07-26 19:54:45 -0300

    first commit

---
+++ ApiCatalogoProdutos/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoProdutos.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
